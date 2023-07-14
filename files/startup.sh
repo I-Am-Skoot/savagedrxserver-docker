2507320291e629c8bf0abb01717762f1f44730e7
I_Am_Skoot <i_am_skoot@users.noreply.github.com>
2023-07-14 11:15:56 -0500

    Updated scripts for more flexibility

--- files/startup.sh
+++ files/startup.sh
@@ -3,6 +3,9 @@
 [[ ! -z "$VIPPASS" ]] && sed -i "s/XXVIPPASSXX/$VIPPASS/g" /var/games/savage-drx/game/startup.cfg || sed -i "s/XXVIPPASSXX/VIP/g" /var/games/savage-drx/game/startup.cfg
 [[ ! -z "$MOTD1" ]] && sed -i "s/XXMOTD1XX/$MOTD1/g" /var/games/savage-drx/game/startup.cfg || sed -i "s/XXMOTD1XX/Welcome to the Docker Hosted Dedicated Server/g" /var/games/savage-drx/game/startup.cfg
 [[ ! -z "$NAME" ]] && sed -i "s/XXNAMEXX/$NAME/g" /var/games/savage-drx/game/startup.cfg || sed -i "s/XXNAMEXX/Docker Savage Server/g" /var/games/savage-drx/game/startup.cfg
+[[ ! -z "$MAXCLIENT" ]] && sed -i "s/XXMAXCLIENTXX/$MAXCLIENT/g" /var/games/savage-drx/game/startup.cfg || sed -i "s/XXMAXCLIENTXX/64/g" /var/games/savage-drx/game/startup.cfg
+[[ ! -z "$PASSWORD" ]] && sed -i "s/XXPASSWORDXX/$PASSWORD/g" /var/games/savage-drx/game/startup.cfg || sed -i "s/XXPASSWORDXX//g" /var/games/savage-drx/game/startup.cfg
+[[ ! -z "$PORT" ]] && sed -i "s/XXPORTXX/$PORT/g" /var/games/savage-drx/game/startup.cfg || sed -i "s/XXPORTXX/11235/g" /var/games/savage-drx/game/startup.cfg
 
 cd /var/games/savage-drx
 ./silverback.bin dedicated_server 1
